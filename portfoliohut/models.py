8f107142645edcd0287e03bac8293cc255b3dd63
Evelyn <eboland148@gmail.com>
2021-03-28 18:15:27 -0400

    Add basic model structure

--- portfoliohut/models.py
+++ portfoliohut/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from djmoney.models.fields import MoneyField
 
 # Create your models here.
+class Investor(models.Model):
+	profile = models.OneToOne(User, on_delete=CASCADE, related_name='profile')
+
+	def __str__(self):
+		return 'Investor: ' + self.profile.get_full_name()
+
+class Transactions(models.Model):
+	investor = models.ForeignKey(Investor, on_delete=PROTECT) 
+	ticker = models.CharField(max_length=10)
+	buy_price = models.DecimalField(decimal_places=2)
+	quantity = models.IntegerFiled(default=-1)
+
+	def __str__(self):
+		return 'Investor: ' + self.investor.profile.get_full_name() +  
+               '\nTicker: ' + self.ticker +  
+               '\nBuy price: ' + self.buy_price +  
+               '\nQuantity: ' + self.quantity 
