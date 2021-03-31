44c5c52fb664344d7da9dcf2b6bc42be77ee1378
Adithya Balaji <adithyabsk@gmail.com>
2021-03-31 14:16:00 -0400

    Update models.

--- portfoliohut/models.py
+++ portfoliohut/models.py
@@ -1,37 +1,50 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-ACTION_CHOICES = (
-    ('buy','BUY'),
-    ('sell', 'SELL'),
+STOCK_ACTIONS = (
+    ("buy", "BUY"),
+    ("sell", "SELL"),
 )
 
-class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+CASH_BALANCE_ACTIONS = (
+    ("withdraw", "WITHDRAW"),
+    ("deposit", "DEPOSIT")
+)
 
-	def __str__(self):
-		return 'Investor: ' + self.user.get_full_name()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    bio = models.CharField(max_length=240)
+    friends = models.ManyToManyField("Profile", blank=True, related_name='friends')
+    friend_requests = models.ManyToManyField("Profile", blank=True, related_name='friend_requests')
+
+    def __str__(self):
+        return "Investor: " + self.user.get_full_name()
+
 
 class CashBalance(models.Model):
-	profile = models.ForeignKey(Profile, on_delete=models.PROTECT) 
-	value = models.DecimalField(max_digits=100, decimal_places=2)
+    action = models.CharField(max_length=4, choices=CASH_BALANCE_ACTIONS)
+    date_time = models.DateTimeField()
+    profile = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    value = models.DecimalField(max_digits=100, decimal_places=2)
 
-	def __str__(self):
-		return 'Profile: ' + self.profile.user.get_full_name() + \
-               '\nValue: ' + self.value 
+    def __str__(self):
+        return "Profile: " + self.profile.user.get_full_name() + \
+               "\nValue: " + self.value
+
 
 class Stock(models.Model):
-	profile = models.ForeignKey(Profile, on_delete=models.PROTECT) 
-	action = models.CharField(max_length=4, choices=ACTION_CHOICES, default='buy')
-	ticker = models.CharField(max_length=10) # must validate
-	date_time = models.DateTimeField()
-	price = models.DecimalField(max_digits=100, decimal_places=2)
-	quantity = models.IntegerField(default=-1)
+    profile = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    action = models.CharField(max_length=4, choices=STOCK_ACTIONS)
+    ticker = models.CharField(max_length=20)  # must validate
+    date_time = models.DateTimeField()
+    price = models.DecimalField(max_digits=100, decimal_places=2)
+    quantity = models.IntegerField()
 
-	def __str__(self):
-		return 'Profile: ' + self.profile.user.get_full_name() + \
-               '\nAction: ' + self.action + \
-               '\nTicker: ' + self.ticker + \
-               '\nDate and Time: ' + self.date_time + \
-               '\nPrice: ' + self.price + \
-               '\nQuantity: ' + self.quantity
+    def __str__(self):
+        return "Profile: " + self.profile.user.get_full_name() + \
+               "\nAction: " + self.action + \
+               "\nTicker: " + self.ticker + \
+               "\nDate and Time: " + self.date_time + \
+               "\nPrice: " + self.price + \
+               "\nQuantity: " + self.quantity
