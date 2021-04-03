c00e636ca0a09d2b4efded9671c56d8d15a2418a
Evelyn <eboland148@gmail.com>
2021-04-03 19:43:42 -0400

    Change follower-stream to friends-compeition and global stream to global-competition

--- portfoliohut/urls.py
+++ portfoliohut/urls.py
@@ -2,14 +2,15 @@
 
 from django.urls import path
 
-from .views import index, login_action, register_action, global_stream, follower_stream, profile_page, logout_action
+from portfoliohut.views import index, login_action, register_action, global_competition, friends_competition, profile_page, logout_action, update_profile
 
 urlpatterns = [
     path("", index, name="index"),
     path("login", login_action, name="login"),
     path("logout", logout_action, name="logout"),
     path("register", register_action, name="register"),
-    path("global-stream", global_stream, name="global-stream"),
-    path("follower-stream", follower_stream, name="follower-stream"),
+    path("global-competition", global_competition, name="global-competition"),
+    path("friends-competition", friends_competition, name="friends-competition"),
     path("profile-page/<int:id_num>", profile_page, name="profile-page"),
+    path("update-profile", update_profile, name="update-profile"),
 ]
