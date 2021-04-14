22b03abbc4052928b5ce4d3955d0bff0cdd24d19
rohanbansal <rohanrb123@gmail.com>
2021-04-14 19:10:07 -0400

    replaced the graph code with Plotly instead of CanvasJS

--- portfoliohut/views/portfolio.py
+++ portfoliohut/views/portfolio.py
@@ -3,7 +3,15 @@ from itertools import chain
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 
+from django.views.generic.base import TemplateView
+
 from portfoliohut.models import Profile, StockTable
+
+from datetime import datetime
+import pandas as pd
+import plotly.offline as opy
+import plotly.graph_objs as go
+import plotly.express as px
 
 
 @login_required
@@ -26,8 +34,37 @@ def portfolio(request):
         dates = []
         for d in list(graph_data.index):
             dates.append(d.strftime("%m/%d/%Y"))
-        returns = list(graph_data)
+        returns = pd.Series(list(graph_data))
+        dates = pd.Series(list(graph_data.index))
+        data = {"Date" : dates,
+                "Returns":returns}
 
+
+        #TODO: Change hardcoded data
+        s_p_returns = returns.multiply(10)
+        data = pd.concat(data, axis = 1)
+        data['User'] = "My portfolio"
+        sp_data = {"Date" : dates,
+                "Returns":s_p_returns}
+        sp_data = pd.concat(sp_data, axis = 1)
+        sp_data['User'] = "Index(S&P 500)"
+        complete_data = pd.concat([data,sp_data], ignore_index=True)
+        fig = px.line(complete_data, x ='Date', y= 'Returns', color='User')
+        fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+        graph = fig.to_html(full_html=False, default_width="90%", default_height="30%")
+        
         return render(
             request,
             "portfoliohut/portfolio.html",
@@ -38,5 +75,6 @@ def portfolio(request):
                 "cash": "${:,.2f}".format(cash_balance),
                 "graph_dates": dates,
                 "graph_returns": returns,
+                'graph': graph
             },
         )
