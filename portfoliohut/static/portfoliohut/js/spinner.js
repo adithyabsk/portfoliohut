673f55aaec461fad1700b8e1996b13f0dd911f21
Evelyn <eboland148@gmail.com>
2021-04-30 16:43:32 -0400

    Add javascript that highlights the logged-in-user's row of the Global and Friends Competition tables

--- portfoliohut/static/portfoliohut/js/spinner.js
+++ portfoliohut/static/portfoliohut/js/spinner.js
@@ -1,7 +1,7 @@
-window.onload = function() {
+window.addEventListener("load",function(){
     $(".spinner-border").fadeOut()
     let content = document.getElementById('content');
     content.classList.remove("invisible")
     let row = document.querySelectorAll('[data-username="{{request.user.username}}"]')[0];
     row.classList.add("bg-light");
-  };
+}, false);
